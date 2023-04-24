8cd065cd8fc2b2a6422628ff8ef832b0088a7816
holabayor <liasu.olabayo@gmail.com>
2023-04-24 13:53:28 +0100

    is going

--- modern_bank_app/src/App.jsx
+++ modern_bank_app/src/App.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import {
+  Billing,
+  Business,
+  CTA,
+  CardDeal,
+  Clients,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from './components';
+import styles from './style';
 
 const App = () => {
   return (
-    <>
-      <div className="bg-primary w-full overflow-hidden">
-        <h1>Hello, World!</h1>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
-    </>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
   );
 };
 
