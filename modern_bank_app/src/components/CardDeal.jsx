8ac318759e01eeabbc7137f06f3cf8f982cd2898
holabayor <liasu.olabayo@gmail.com>
2023-04-25 11:41:09 +0100

    done

--- modern_bank_app/src/components/CardDeal.jsx
+++ modern_bank_app/src/components/CardDeal.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { card } from '../assets';
+import styles, { layout } from '../style';
+import Button from './Button';
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal
+        <br className="sm:block hidden" />
+        in few easy steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+    <div className={`${layout.sectionImg} flex-col`}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
 
 export default CardDeal;
